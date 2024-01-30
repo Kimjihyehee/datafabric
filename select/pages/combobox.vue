6f67b96aaee143950148d1b7503e7fff61dea85d
김지혜 <kimjihye6245@naver.com>
2024-01-30 16:58:39 +0900

    feat: 콤보박스 개발세팅

---
+++ select/pages/combobox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h1>Combo Box Example</h1>
+      <combobox></combobox>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Combobox from '../components/combobox.vue'
+</script>
+
+<style scoped>
+
+</style>
