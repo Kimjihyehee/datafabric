b4a474161d71a14e846f2c4604af02715ef5413d
김지혜 <kimjihye6245@naver.com>
2024-01-30 17:33:49 +0900

    feat: props선언 & 콤보박스 클릭시, 리스트 보이기 기능추가

--- select/components/combobox.vue
+++ select/components/combobox.vue
@@ -1,6 +1,55 @@
 <template>
-  <p>예시</p>
+  <div class="combo-box" style="width: 300px; height: 30px;">
+      <input type="text" @click="toggleList"></input>
+      <ul v-show="isShowBox">
+          <li>선택1</li>
+          <li>선택2</li>
+          <li>선택3</li>
+      </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
+
+// 드롭다운 활성화 상태
+const isShowBox = ref(false);
+
+// props로 받은 옵션 목록값
+const props = defineProps({
+    data: {
+        type: Array,
+    },
+    labelKey: {
+        type: [String, Number],
+        default: "label"
+    },
+    valueKey: {
+        type: String,
+        default: "value"
+    },
+    disabled: {
+        type: Boolean,
+    },
+    placeholder: {
+        type: String,
+        default: "",
+    },
+    disabledList: {
+        type: Array<String | Number>,
+        default: [],
+    },
+    nodataMsg: {
+        type: String,
+    },
+    noSearchMsg: {
+        type: String
+    }
+
+})
+
+// 콤보박스 항목 선택시, 실행되는 함수
+function toggleList() {
+    isShowBox.value = !isShowBox.value;
+    console.log(isShowBox.value)
+}
 </script>
