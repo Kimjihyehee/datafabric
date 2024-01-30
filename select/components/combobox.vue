17949f374120839676d26882c7ac775e9b89b543
김지혜 <kimjihye6245@naver.com>
2024-01-30 17:48:11 +0900

    feat:외부클릭시 콤보박스 드롭다운 보이지 않도록 처리

--- select/components/combobox.vue
+++ select/components/combobox.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="combo-box" style="width: 300px; height: 30px;">
       <input type="text" @click="toggleList"></input>
-      <ul v-show="isShowBox">
+      <ul v-show="isShowBox" v-on-click-outside="closeDropdown">
           <li>선택1</li>
           <li>선택2</li>
           <li>선택3</li>
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-
+import { vOnClickOutside } from '@vueuse/components'
 // 드롭다운 활성화 상태
 const isShowBox = ref(false);
 
@@ -52,4 +52,10 @@ function toggleList() {
     isShowBox.value = !isShowBox.value;
     console.log(isShowBox.value)
 }
+
+// 외부 클릭 시, 드롭다운이 닫히는 함수
+const closeDropdown = () => {
+    isShowBox.value = false;
+}
+
 </script>
