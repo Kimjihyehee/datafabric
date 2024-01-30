1b3ecbae76746a45296b2fb0987d44e64e1509fd
김지혜 <kimjihye6245@naver.com>
2024-01-30 17:58:44 +0900

    feat: 콤보박스 항목 클릭시, 선택한 값을 가지도록 하는 기능

--- select/pages/combobox.vue
+++ select/pages/combobox.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
       <h1>Combo Box Example</h1>
-      <combobox></combobox>
+      <combobox :data="options" label-key="key" value-key="value" @select="selectItem"></combobox>
   </div>
 </template>
 
 <script setup lang="ts">
 import Combobox from '../components/combobox.vue'
+
+const options = ref(
+    [
+        {
+            key: "option1",
+            value: "option1",
+        },
+        {
+            key: "option2",
+            value: "option2",
+        },
+        {
+            key: "option3",
+            value: "option3",
+        },
+    ]
+)
+function selectItem(value) {
+    console.log(value);
+}
+
 </script>
 
 <style scoped>
