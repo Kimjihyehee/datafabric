b2322d2bc70439d53d848ae307acfe60e4eb1b1c
김지혜 <kimjihye6245@naver.com>
2024-01-30 15:39:11 +0900

    feat:셀렉트박스 컴포넌트 개발

---
+++ select/pages/selectbox.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Select Box Example</h1>
+        <SelectBox :data="options" label-key="key" value-key="value" @select="selectItem" :disabled="false"
+                   :disableList="disableList" nodataMsg="'메시지가 없습니다.'">
+        </SelectBox>
+    </div>
+</template>
+<script setup lang="ts">
+import SelectBox from '../components/selectbox.vue';
+
+// disable 항목리스트
+const disableList = ref([
+    'option1', 'option2'
+]);
+// 드롭다운 항목에 들어갈 옵션데이터
+const options = ref(
+    [
+        {
+            key: "option1",
+            value: "option1",
+        },
+        {
+            key: "option2",
+            value: "option2",
+        },
+        {
+            key: "option3",
+            value: "option3",
+        },
+    ]
+);
+
+function selectItem(value) {
+    console.log(value)
+}
+
+</script>
