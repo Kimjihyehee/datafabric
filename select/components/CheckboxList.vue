ee25320ab82aa35acc5bea26ac0e4cb45dd9d62b
김지혜 <kimjihye6245@naver.com>
2024-02-07 17:07:18 +0900

    feat: data, checkboxIds 선언 및 props세팅

--- select/components/CheckboxList.vue
+++ select/components/CheckboxList.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>컴포넌트</div>
+  <div class="checkbox">
+      <input
+        type="checkbox"
+      />
+      <label></label>
+  </div>
 </template>
 
-<script>
-export default {
-    name: "CheckboxList.vue"
-}
+<script setup lang="ts">
+
+const props = defineProps({
+    checkboxIds: {
+        type: Array<string>,
+        default: []
+    },
+    data: {
+        type: Array<{ [key: string]: string | number }>,
+    },
+    labelKey: {
+        type: String,
+        default: "label"
+    },
+    valueKey: {
+        type: String,
+        default: "value"
+    }
+})
+
+/** TODO :(로직구현!_) data.length가 넘어온 checkboxIds의 값과 같지 않다면,
+   checkboxIds의 값을 onmounted 때, uuid생성해서 넣어주도록 한다. **/
+
 </script>
 
 <style scoped>
--- select/pages/CheckboxList.vue
+++ select/pages/CheckboxList.vue
@@ -1,11 +1,35 @@
 <template>
-    <p>페이지 생성</p>
-    <checkbox-List></checkbox-List>
-    <p>예시</p>
+    <div>
+        <h1>checkBox Example</h1>
+        <checkbox-List :data="options" label-Key="key" value-Key="value"
+        :checkboxIds="checkboxIds"></checkbox-List>
+    </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import CheckboxList from "@/components/CheckboxList.vue";
+
+const checkboxIds = ref([
+    'firstId', 'secondId', 'thirdId'
+])
+
+const options = ref(
+    [
+        {
+            key: "option1",
+            value: "option11",
+        },
+        {
+            key: "option2",
+            value: "option22",
+        },
+        {
+            key: "option3",
+            value: "option33",
+        },
+    ]
+);
+
 </script>
 
 <style scoped>
