699573b569908229222e5c9a27194f86505648da
김지혜 <kimjihye6245@naver.com>
2024-02-19 16:37:15 +0900

    feat: 체크박스리스트 항목 UUID로 id할당하여 보여주기

--- select/components/CheckboxList.vue
+++ select/components/CheckboxList.vue
@@ -1,11 +1,51 @@
 <template>
-  <div>컴포넌트</div>
+  <div class="checkbox">
+    <div v-for="(option, index) in checkboxList" :key="index">
+      <input
+        type="checkbox"
+        :id="option.id"
+        :value="option.value"
+      />
+      <label :for="option.id">{{ option.label }}</label>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-    name: "CheckboxList.vue"
-}
+<script setup lang="ts">
+import { uuid } from 'vue-uuid';
+
+const props = defineProps({
+    data: {
+        type: Array<{ [key: string]: string | number }>,
+        default: []
+    },
+    labelKey: {
+        type: String,
+        default: "label"
+    },
+    valueKey: {
+        type: String,
+        default: "value"
+    }
+})
+
+const checkboxList = computed(() => {
+    const labelKey = props.labelKey;
+    const valuekey = props.valueKey;
+
+    return reactive(props.data.map(value => {
+
+        return {
+            id: uuid.v4(),
+            label: value[labelKey],
+            value: value[valuekey],
+        }
+    }));
+});
+
+const emit = defineEmits<{ (e: "change", item: Array<String | Number>): void }>();
+
+
 </script>
 
 <style scoped>
--- select/pages/CheckboxList.vue
+++ select/pages/CheckboxList.vue
@@ -1,11 +1,38 @@
 <template>
-    <p>페이지 생성</p>
-    <checkbox-List></checkbox-List>
-    <p>예시</p>
+    <div>
+        <h1>RecheckboxList Example</h1>
+        <checkbox-List :data="options" label-key="key" value-key="value"></checkbox-List>
+    </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import CheckboxList from "@/components/CheckboxList.vue";
+
+const options = ref(
+    [
+        {
+            key: "option1",
+            value: "option11",
+        },
+        {
+            key: "option2",
+            value: "option22",
+        },
+        {
+            key: "option3",
+            value: "option33",
+        },
+        {
+            key: "option4",
+            value: "option44",
+        },
+        {
+            key: "option5",
+            value: "option55",
+        },
+    ]
+);
+
 </script>
 
 <style scoped>
