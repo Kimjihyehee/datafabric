85279792cd0956130f849b7d556b72969077f13f
김지혜 <kimjihye6245@naver.com>
2024-02-28 09:16:18 +0900

    style: 불필요한 코드 수정 및 타입 지정 & package.json 내용 추가

--- select/components/selectbox.vue
+++ select/components/selectbox.vue
@@ -11,9 +11,9 @@
 import { vOnClickOutside } from '@vueuse/components'
 
 // 드롭다운 활성화 상태
-const isShowBox = ref(false);
+const isShowBox = ref<boolean>(false);
 // 선택된 값의 라벨
-const selectedLabel = ref("선택");
+const selectedLabel = ref<string | number>("선택");
 
 // props로 받은 옵션 목록값
 const props = defineProps({
@@ -44,7 +44,7 @@ const props = defineProps({
 });
 
 // 선택여부를 확인하는 함수
-const isActive = (value): boolean => {
+const isActive = (value:string): boolean => {
     return value === selectedValue.value;
 }
 
@@ -54,7 +54,7 @@ if (!props.data?.length) {
 }
 const emit = defineEmits<{ (e: "select", option: number | string): void }>();
 // disable 적용
-const isDisabled = (value): boolean => {
+const isDisabled = (value:string): boolean => {
     // disableList에 속하는 경우, T를 리턴
     return props.disableList.includes(value);
 }
@@ -64,15 +64,15 @@ function toggleList(): void {
     isShowBox.value = !isShowBox.value;
 }
 // 셀렉트박스의 항목을 선택시, 실행되는 함수
-function selectItem(option): void {
-    const selectedValueKey = option[props.valueKey];
+function selectItem(option:object[]): void {
+    const selectedValueKey:string = option[props.valueKey];
     selectedLabel.value = option[props.labelKey];
     emit("select", selectedValueKey)
     selectedValue.value = selectedValueKey; // 선택된 값을 selectedValue로 저장
     closeDropdown();
 }
 
-const selectedValue = ref(props.selectedItem);
+const selectedValue = ref<string | number>(props.selectedItem);
 
 if (props.selectedItem !== undefined) {
     // props.selectedItem이 undefined 아닐 때, 해당 값이 props.data 배열에 포함되어 있는지 확인
@@ -80,7 +80,9 @@ if (props.selectedItem !== undefined) {
         option[props.valueKey] === props.selectedItem
     );
     if(foundItem) { // foundItem의 값이 존재할 경우
-        selectItem(foundItem);
+        const selectedValueKey = foundItem[props.valueKey];
+        selectedLabel.value = foundItem[props.labelKey];
+        selectedValue.value = selectedValueKey; // 선택된 값을 selectedValue로 저장
     }
 }
 // 외부 클릭 시, 드롭다운이 닫히는 함수
